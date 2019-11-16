88f0ebe7d71083e31e7ed2136b9de98549192890
Enderedne <nevelichkoff@gmail.com>
2019-11-16 23:48:49 +0200

    Seperate fridge item

--- src/components/Fridge/Fridge.js
+++ src/components/Fridge/Fridge.js
@@ -1,20 +1,18 @@
-import Avatar from '@material-ui/core/Avatar';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import ListItemText from '@material-ui/core/ListItemText';
-import Typography from '@material-ui/core/Typography';
-import FolderIcon from '@material-ui/icons/Folder';
-import DeleteIcon from '@material-ui/icons/Delete';
+import UndoIcon from '@material-ui/icons/Undo';
 import useAxios from 'axios-hooks';
 import React, { useState } from 'react';
+import FridgeItem from './FridgeItem';
 import useStyles from './useStyles';
 
 const Fridge = () => {
   const [{ data: products, loading, error }] = useAxios('/products');
+  const [{ data, loading, error }, returnToFridge] = useAxios(
+    { url: `products`, method: 'put' },
+    { manual: true }
+  );
 
   const [term, setTerm] = useState('');
   const classes = useStyles();
@@ -22,6 +20,15 @@ const Fridge = () => {
   if (loading || error) {
     return null;
   }
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+    try {
+      await returnToFridge({ data: { shared: false } });
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   return (
     <div>
@@ -31,35 +38,12 @@ const Fridge = () => {
           .filter(product => product.name.toLowerCase().includes(term.toLowerCase()))
           .map(product => (
             <div key={product.id}>
-              <ListItem alignItems="flex-start">
-                <ListItemAvatar>
-                  <Avatar
-                    alt={product.name.toLowerCase() + '.svg'}
-                    src={'/icon_pack/svg/' + product.name.toLowerCase() + '.svg'}
-                  />
-                </ListItemAvatar>
-                <ListItemText
-                  primary={product.name}
-                  secondary={
-                    <React.Fragment>
-                      <Typography
-                        component="span"
-                        variant="body2"
-                        className={classes.inline}
-                        color="textPrimary"
-                      >
-                        Quantity: {product.quantity}
-                      </Typography>
-                      {' — by ' + product.creator.fullName}
-                    </React.Fragment>
-                  }
-                />
-                <ListItemSecondaryAction>
-                  <IconButton edge="end" aria-label="delete">
-                    <DeleteIcon />
-                  </IconButton>
-                </ListItemSecondaryAction>
-              </ListItem>
+              <FridgeItem product={product}>
+                <IconButton edge="end" aria-label="undo" onClick={handleSubmit}>
+                  <UndoIcon />
+                </IconButton>
+              </FridgeItem>
+              {/* </Swipeable> */}
               <Divider variant="inset" component="li" />
             </div>
           ))}
---
+++ src/components/Fridge/FridgeItem.js
@@ -0,0 +1,47 @@
+import Avatar from '@material-ui/core/Avatar';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Typography from '@material-ui/core/Typography';
+import React from 'react';
+import useStyles from './useStyles';
+
+const FridgeItem = ({ product, children }) => {
+    const classes = useStyles();
+
+    return (
+        <ListItem
+            alignItems="flex-start"
+        // style={"transform:translate(${})"}
+        >
+            <ListItemAvatar>
+                <Avatar
+                    alt={product.name.toLowerCase() + '.svg'}
+                    src={'/icon_pack/svg/' + product.name.toLowerCase() + '.svg'}
+                />
+            </ListItemAvatar>
+            <ListItemText
+                primary={product.name}
+                secondary={
+                    <React.Fragment>
+                        <Typography
+                            component="span"
+                            variant="body2"
+                            className={classes.inline}
+                            color="textPrimary"
+                        >
+                            Quantity: {product.quantity}
+                        </Typography>
+                        {' — by ' + product.creator.fullName}
+                    </React.Fragment>
+                }
+            />
+            <ListItemSecondaryAction>
+                {children}
+            </ListItemSecondaryAction>
+        </ListItem>
+    );
+}
+
+export default FridgeItem;
