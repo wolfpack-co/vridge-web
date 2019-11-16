5da77b1e5b5952844a419c68f4173318bf4c28aa
Vladimir Aleksiev <vladimir.aleksiev@tailify.com>
2019-11-16 03:35:38 +0200

    add azure-pipelines.yml

--- src/components/RequestForm/RequestForm.js
+++ src/components/RequestForm/RequestForm.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import ReactNotifications from 'react-browser-notifications';
 import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
 import Fab from '@material-ui/core/Fab';
 import CheckIcon from '@material-ui/icons/Check';
 import CrossIcon from '@material-ui/icons/Clear';
@@ -11,21 +11,30 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
+let message = '';
 const RequestForm = ({ product }) => {
+  let ref = useRef(null);
+  let message = useRef('');
+
   const classes = useStyles();
 
   return (
     <div>
+      <ReactNotifications
+        onRef={r => (ref = r)}
+        title="message" // Required
+        body="This is the body"
+        icon="icon.png"
+        tag="abcdef"
+        timeout="2000"
+        onClick={event => ref.close(event.target.tag)}
+      />
+
       <h2 id="transition-modal-title">{product.name}</h2>
       <p id="transition-modal-description">
         {product.creator.name} - fl. {product.creator.floor} ap. {product.creator.apartment}{' '}
       </p>
-      <Fab
-        color="primary"
-        aria-label="add"
-        className={classes.fab}
-        onClick={() => console.log('requesting')}
-      >
+      <Fab color="primary" aria-label="add" className={classes.fab} onClick={() => ref.show()}>
         <CheckIcon />
       </Fab>
       <Fab
