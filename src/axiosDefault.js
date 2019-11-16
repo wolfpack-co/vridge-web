12f2e042415f1a48ec71a34092b5b9309958c0ad
Enderedne <nevelichkoff@gmail.com>
2019-11-16 22:16:32 +0200

    Add icon pack, update fridge list

--- src/axiosDefault.js
+++ src/axiosDefault.js
@@ -1,3 +1,3 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://vridge.app/api/';
+axios.defaults.baseURL = 'http://vridge-api.azurewebsites.net/api';
--- src/components/Fridge/Fridge.js
+++ src/components/Fridge/Fridge.js
@@ -1,103 +1,108 @@
-import React, { useState } from 'react';
-import useAxios from 'axios-hooks';
-import Container from '@material-ui/core/Container';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
-import ListSubheader from '@material-ui/core/ListSubheader';
-import Search from '@material-ui/icons/Search';
-import TextField from '@material-ui/core/TextField';
-import InputAdornment from '@material-ui/core/InputAdornment';
 import Avatar from '@material-ui/core/Avatar';
-import GetButton from '../GetButton';
-import LeaveButton from '../LeaveButton';
-
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Typography from '@material-ui/core/Typography';
+import FolderIcon from '@material-ui/icons/Folder';
+import DeleteIcon from '@material-ui/icons/Delete';
+import useAxios from 'axios-hooks';
+import React, { useState } from 'react';
 import useStyles from './useStyles';
 
-const products = [
-  {
-    img: 'tomato.png',
-    name: 'Tomatoes',
-    creator: {
-      name: 'Milko Mitropolitsky',
-      floor: 5,
-      apartment: 12,
-    },
-  },
-  {
-    img: 'cucumber.jpeg',
-    name: 'Cucumber',
-    author: 'author',
-    creator: {
-      name: 'Milko Mitropolitsky',
-      floor: 5,
-      apartment: 12,
-    },
-  },
-  {
-    img: 'eggs.png',
-    name: 'Eggs',
-    author: 'author',
-    creator: {
-      name: 'Milko Mitropolitsky',
-      floor: 5,
-      apartment: 12,
-    },
-  },
-];
+
+// const products = [
+//   {
+//     img: 'icon_pack/svg/tomato.svg',
+//     name: 'Tomatoes',
+//     quantity: 5,
+//     creator: {
+//       name: 'Milko Mitropolitsky',
+//       floor: 5,
+//       apartment: 12,
+//     },
+//   },
+//   {
+//     img: 'icon_pack/svg/cucumber.svg',
+//     name: 'Cucumber',
+//     author: 'author',
+//     quantity: 2,
+//     creator: {
+//       name: 'Milko Mitropolitsky',
+//       floor: 5,
+//       apartment: 12,
+//     },
+//   },
+//   {
+//     img: 'icon_pack/svg/eggs.svg',
+//     name: 'Eggs',
+//     author: 'author',
+//     quantity: 3,
+//     creator: {
+//       name: 'Milko Mitropolitsky',
+//       floor: 5,
+//       apartment: 12,
+//     },
+//   },
+// ];
 
 const Fridge = () => {
-  const [{ data, loading, error }] = useAxios('/products');
+  const [{ data: products, loading, error }] = useAxios('/products');
   const [term, setTerm] = useState('');
   const classes = useStyles();
 
-  return (
-    <Container maxWidth="sm">
-      <GridList cellHeight={180} className={classes.gridList} cols={2}>
-        <GridListTile key="Subheader" cols={2} style={{ height: 'auto', fontSize: '26px' }}>
-          <ListSubheader className={classes.header} component="h3">
-            <span className={classes.heading}>What's available</span>
-            <LeaveButton />
-          </ListSubheader>
-        </GridListTile>
-        <GridListTile key="Search" cols={3} style={{ height: 'auto' }}>
-          <TextField
-            className={classes.search}
-            id="input-with-icon-textfield"
-            label="Search"
-            onChange={event => setTerm(event.target.value)}
-            value={term}
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <Search />
-                </InputAdornment>
-              ),
-            }}
-          />
-        </GridListTile>
+  if (loading || error) {
+    return null;
+  }
 
+  debugger;
+
+  return (
+    <div>
+      {/* <Container maxWidth="sm"> */}
+      <List className={classes.root}>
         {products
           .filter(product => product.name.toLowerCase().includes(term.toLowerCase()))
           .map(product => (
-            <GridListTile key={product.img} cols={1}>
-              <Avatar
-                alt="Remy Sharp"
-                src="https://material-ui.com/static/images/avatar/1.jpg"
-                className={classes.avatar}
-              />
-
-              <img src={product.img} alt={product.name} />
-              <GridListTileBar
-                className={classes.title}
-                title={product.name}
-                subtitle={<span>by: {product.creator.name}</span>}
-                actionIcon={<GetButton product={product} />}
-              ></GridListTileBar>
-            </GridListTile>
+            <div>
+              <ListItem alignItems="flex-start">
+                <ListItemAvatar>
+                  <Avatar
+                    alt={product.name.toLowerCase() + ".svg"}
+                    src={"icon_pack/svg/" + product.name.toLowerCase() + ".svg"}
+                    className={classes.avatar}
+                  />
+                </ListItemAvatar>
+                <ListItemText
+                  primary={product.name}
+                  secondary={
+                    <React.Fragment>
+                      <Typography
+                        component="span"
+                        variant="body2"
+                        className={classes.inline}
+                        color="textPrimary"
+                      >
+                        Quantity: {product.quantity}
+                      </Typography>
+                      {" — by " + product.creator.fullName}
+                    </React.Fragment>
+                  }
+                />
+                <ListItemSecondaryAction>
+                  <IconButton edge="end" aria-label="delete">
+                    <DeleteIcon />
+                  </IconButton>
+                </ListItemSecondaryAction>
+              </ListItem>
+              <Divider variant="inset" component="li" />
+            </div>
           ))}
-      </GridList>
-    </Container>
+      </List>
+    </div>
   );
 };
 
