315bd950d76068b89a2c1f23f71af1da25cd7fd6
Enderedne <nevelichkoff@gmail.com>
2019-11-17 07:30:27 +0200

    wire route for product creation

--- src/components/AddToFridgeForm/AddToFridgeForm.js
+++ src/components/AddToFridgeForm/AddToFridgeForm.js
@@ -1,17 +1,17 @@
-import React, { useState } from 'react';
-import useAxios from 'axios-hooks';
-import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
+import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
-import Fab from '@material-ui/core/Fab';
 import CheckIcon from '@material-ui/icons/Check';
 import CrossIcon from '@material-ui/icons/Clear';
+import useAxios from 'axios-hooks';
+import { useHistory } from 'react-router-dom';
+import React, { useState } from 'react';
 
-const products = ['tomatoes', 'cucumber', 'eggs', 'pork butt'];
+const products = ["apple", "asparagus", "aubergine", "avocado", "bacon", "baguette", "banana", "beans", "biscuit", "blueberries", "boiled", "bowl", "bread", "broccoli", "butcher", "butter", "cabbage", "cake", "can", "candy", "carrot", "cauliflower", "cereals", "cheese", "chef", "cherries", "chili", "chips", "chives", "chocolate", "coconut", "coffee", "cookies", "corckscrew", "corn", "corndog", "croissant", "cucumber", "cup", "cupcake", "cutlery", "dairy", "dish", "dishes", "doughnut", "egg", "eggs", "fig", "fish", "flour", "food", "fork", "frappe", "fries", "garlic", "gingerbread", "glass", "grain", "grapes", "grater", "grinder", "groceries", "ham", "hamburguer", "hazelnut", "honey", "jam", "jawbreaker", "jelly", "kebab", "kitchen", "knife", "knives", "ladle", "lemon", "lime", "meat", "milk", "mixer", "mug", "mushroom", "mushrooms", "mustard", "noodles", "oat", "octopus", "oil", "olives", "onion", "orange", "ornating", "pan", "pancakes", "pasta", "peach", "pear", "peas", "pepper", "pickles", "pie", "pineapple", "pint", "pistachio", "pizza", "pomegranate", "popsicle", "pot", "potatoes", "pretzel", "pudding", "pumpkin", "radish", "raspberry", "rice", "risotto", "salad", "salami", "salmon", "salt", "sandwich", "sausage", "scale", "seeds", "shrimp", "sorbet", "spaguetti", "spatula", "spices", "spoon", "steak", "stew", "strainer", "strawberry", "sushi", "taco", "tea", "teapot", "teaspoon", "tenderizer", "thermos", "toast", "toaster", "toffee", "tomato", "turkey", "water", "watermelon", "whisk", "wrap"];
 
 const useStyles = makeStyles(theme => ({
   formControl: {
@@ -24,17 +24,21 @@ const useStyles = makeStyles(theme => ({
   },
   productName: {
     textTransform: 'capitalize',
-  },
-  actions: {
-    textAlign: 'center',
-  },
-  fab: { margin: '0 10px' },
+  }
 }));
 
 const AddToFridgeForm = ({ onDismiss }) => {
   const classes = useStyles();
   const [name, setName] = useState('');
   const [quantity, setQuantity] = useState(0);
+
+  const history = useHistory();
+  const [value, setValue] = React.useState(history.location.pathname);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+    history.push(newValue);
+  };
 
   const [{ data, loading, error }, leaveInFridge] = useAxios(
     { url: `products`, method: 'post' },
@@ -44,7 +48,8 @@ const AddToFridgeForm = ({ onDismiss }) => {
   const handleSubmit = async event => {
     event.preventDefault();
     try {
-      await leaveInFridge({ data: { shared: true, name, quantity, creator: { id: 1 } } });
+      await leaveInFridge({ data: { shared: false, name, quantity, creator: { id: localStorage.getItem('user') } } });
+      handleChange(event, '/fridge')
     } catch (e) {
       console.log(e);
     }
@@ -77,13 +82,13 @@ const AddToFridgeForm = ({ onDismiss }) => {
           value={quantity}
         />
       </FormControl>
-      <div className={classes.actions}>
-        <Fab color="primary" type="submit" aria-label="add" className={classes.fab}>
+      <div>
+        <Button id="btnSubmit" disabled={!(name || quantity)} variant="outlined" type="submit" color="primary" className={classes.button}>
           <CheckIcon />
-        </Fab>
-        <Fab color="default" aria-label="add" className={classes.fab} onClick={onDismiss}>
+        </Button>
+        <Button variant="outlined" color="secondary" className={classes.button} onClick={onDismiss}>
           <CrossIcon />
-        </Fab>
+        </Button>
       </div>
     </form>
   );
--- src/pages/FridgePage.js
+++ src/pages/FridgePage.js
@@ -1,10 +1,67 @@
-import React from 'react';
-import AuthorizedLayout from './AuthorizedLayout';
+import { green } from '@material-ui/core/colors';
+import Fab from '@material-ui/core/Fab';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Zoom from '@material-ui/core/Zoom';
+import AddIcon from '@material-ui/icons/Add';
+import clsx from 'clsx';
+import { useHistory } from 'react-router-dom';
+import React, { useState } from 'react';
 import Fridge from '../components/Fridge/Fridge';
+import AuthorizedLayout from './AuthorizedLayout';
 
-const FridgePage = () => 
-<AuthorizedLayout>
-    <Fridge />
-</AuthorizedLayout>;
+const useStyles = makeStyles(theme => ({
+    root: {
+        position: 'relative'
+    },
+    fab: {
+        color: 'primary',
+        position: 'absolute',
+        bottom: theme.spacing(12),
+        right: theme.spacing(2),
+    },
+}));
+
+const FridgePage = () => {
+    const classes = useStyles();
+    const theme = useTheme();
+    const history = useHistory();
+    const [value, setValue] = React.useState(history.location.pathname);
+
+    const transitionDuration = {
+        enter: theme.transitions.duration.enteringScreen,
+        exit: theme.transitions.duration.leavingScreen,
+    };
+
+    const fab = {
+        color: 'primary',
+        className: clsx(classes.fab),
+        icon: <AddIcon />,
+        label: 'Add',
+    };
+
+    const handleChange = () => {
+        setValue('/fridge/new');
+        history.push('/fridge/new');
+    };
+
+    return (
+        <AuthorizedLayout className={classes.root} >
+            <Fridge />
+            <Zoom
+                key={fab.color}
+                in={value === '/fridge'}
+                timeout={transitionDuration}
+                style={{
+                    transitionDelay: `${value === '/fridge' ? transitionDuration.exit : 0}ms`,
+                }}
+                unmountOnExit
+            >
+                <Fab aria-label={fab.label} className={fab.className} color={fab.color} onClick={handleChange}>
+                    {fab.icon}
+                </Fab>
+            </Zoom>
+        </AuthorizedLayout >
+    );
+}
 
 export default FridgePage;
