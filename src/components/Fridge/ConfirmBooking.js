d91b710d85de5e18e2e81674f949ac9a8c930be5
Vladimir Aleksiev <vladimir.aleksiev@tailify.com>
2019-11-17 10:00:05 +0200

    add customer name label

--- src/components/Fridge/ConfirmBooking.js
+++ src/components/Fridge/ConfirmBooking.js
@@ -4,27 +4,30 @@ import useAxios from 'axios-hooks';
 import React from 'react';
 
 const ConfirmBooking = ({ product, refetch }) => {
-    // actions the user can perform on foreign product
-    const [, confirmBooking] = useAxios(
-        { url: `products/${product.id}/confirm`, method: 'put' },
-        { manual: true }
-    );
+  // actions the user can perform on foreign product
+  const [, confirmBooking] = useAxios(
+    { url: `products/${product.id}/confirm`, method: 'put' },
+    { manual: true }
+  );
 
-    const handleConfirmation = async event => {
-        event.preventDefault();
-        try {
-            await confirmBooking({ data: { } });
-            refetch();
-        } catch (e) {
-            console.log(e);
-        }
-    };
+  const handleConfirmation = async event => {
+    event.preventDefault();
+    try {
+      await confirmBooking({ data: {} });
+      refetch();
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
-    return (
-        <IconButton edge="end" aria-label="confirm" onClick={handleConfirmation}>
-            <CheckIcon />
-        </IconButton>
-    );
+  return (
+    <>
+      <span style={{ fontSize: '9px' }}>{product.consumer.fullName.split(' ')[0]}</span>
+      <IconButton edge="end" aria-label="confirm" onClick={handleConfirmation}>
+        <CheckIcon />
+      </IconButton>
+    </>
+  );
 };
 
 export default ConfirmBooking;
